3ad7317c955e7955a0ea8f5220e330d562b692e0
Matija Lukanić <matija.lukanic@wem.hr>
2024-12-22 12:08:35 +0100

    Tag implemented in mvc. Tested and working. Todo: ItemTag

---
+++ Marketplace/MarketplaceMvc/ViewModels/TagVM.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MarketplaceMvc.ViewModels
+{
+    public class TagVM
+    {
+        [Display(Name = "Tag")]
+        [Required(ErrorMessage = "Enter something in here")]
+        public int TagId { get; set; }
+
+        [Display(Name = "Tag")]
+        [Required(ErrorMessage = "Enter something in here")]
+        public string? TagName { get; set; }
+    }
+}
